47cbc310d98e6ee84a24a873d614cade7244cd71
skarev <sevenkss95@gmail.com>
2020-08-26 14:03:42 +0300

    Initial Commit

---
+++ Alarstudios test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alarstudios test
+//
+//  Created by skarev on 26.08.2020.
+//  Copyright © 2020 skarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
